530ebdfd7ef42ca85e9a66030df57aca9363aa63
MudOnTire <895157882@qq.com>
2021-01-31 09:49:26 +0800

    update

--- src/components/SettingDrawer/index.js
+++ src/components/SettingDrawer/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Drawer } from 'antd';
 
-export default function SettingDrawer({ }) {
+export default function SettingDrawer({ visible, onClose }) {
   return (
-    <div>
-
-    </div>
+    <Drawer
+      title="Settings"
+      placement="right"
+      closable={true}
+      onClose={onClose}
+      visible={visible}
+    >
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </Drawer>
   )
 }
--- src/pages/Editor/index.js
+++ src/pages/Editor/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CompList from 'Src/components/CompList';
 import Draft from 'Src/components/Draft';
+import SettingDrawer from 'Src/components/SettingDrawer';
 
 import styles from './styles.module.scss';
 
 export default function Editor() {
+
+  const [showSettingDrawer, setShowSettingDrawer] = useState(true);
+
   return (
     <div className={styles.editor}>
       <CompList />
       <Draft />
+      <SettingDrawer
+        visible={showSettingDrawer}
+        onClose={() => { setShowSettingDrawer(false); }}
+      />
     </div>
   )
 }
