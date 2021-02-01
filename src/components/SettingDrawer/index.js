7e3d3bc54dd31c15bd7be6d69108048080782a0a
bruce_zhang <bruce_zhang@trendmicro.com>
2021-02-01 12:16:36 +0800

    update setting drawer

--- src/components/SettingDrawer/index.js
+++ src/components/SettingDrawer/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import { Drawer } from 'antd';
+import React, { useContext } from 'react';
+import { Button } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
+import { context, actions } from 'Src/store';
 
-export default function SettingDrawer({ visible, onClose }) {
+import styles from './styles.module.scss';
+
+export default function SettingDrawer() {
+
+  const store = useContext(context);
+  const { dispatch, configedContainerId } = store;
+
+  const close = () => {
+    dispatch({
+      type: actions.SET_CONFIG_CONTAINER,
+      payload: null
+    });
+  }
+
   return (
-    <Drawer
-      title="Settings"
-      placement="right"
-      closable={true}
-      onClose={onClose}
-      visible={visible}
-    >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-    </Drawer>
+    <div className={`${styles.settingDrawer} ${configedContainerId ? styles.visible : styles.hidden}`}>
+      <div className={styles.content}>
+        this is content
+      </div>
+      <Button
+        type="text"
+        className={styles.closeBtn}
+        icon={<CloseOutlined />}
+        onClick={close}
+      />
+    </div>
   )
 }
--- src/pages/Editor/index.js
+++ src/pages/Editor/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react';
 import CompList from 'Src/components/CompList';
 import Draft from 'Src/components/Draft';
 import SettingDrawer from 'Src/components/SettingDrawer';
@@ -7,16 +7,11 @@ import styles from './styles.module.scss';
 
 export default function Editor() {
 
-  const [showSettingDrawer, setShowSettingDrawer] = useState(true);
-
   return (
     <div className={styles.editor}>
       <CompList />
       <Draft />
-      <SettingDrawer
-        visible={showSettingDrawer}
-        onClose={() => { setShowSettingDrawer(false); }}
-      />
+      <SettingDrawer />
     </div>
   )
 }
