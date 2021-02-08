1f5b4b8c8d9aa7abd80c1cb2cfdc35e4d10044e6
bruce_zhang <bruce_zhang@trendmicro.com>
2021-02-08 18:12:42 +0800

    update table

--- src/widgets/data_display/Table/index.js
+++ src/widgets/data_display/Table/index.js
@@ -1,5 +1,5 @@
-import React, { useMemo, useEffect } from 'react';
-import { Table as AntTable, Tag, Space } from 'antd';
+import React, { useMemo, useEffect, useState } from 'react';
+import { Table as AntTable } from 'antd';
 import WidgetWrapper from 'Src/components/WidgetWrapper';
 import defaultSettings from 'Src/config/defaultSettings';
 import settingSchemas from 'Src/config/settingSchemas';
@@ -7,78 +7,30 @@ import request from 'Src/uitls/request';
 
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a>{text}</a>,
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id'
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: 'User Id',
+    dataIndex: 'userId',
+    key: 'userId'
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title',
   },
   {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
+    title: 'Body',
+    dataIndex: 'body',
+    key: 'body',
+  }
 ];
 
 export default function Image({ config }) {
+
+  const [data, setData] = useState([]);
 
   const finalSettings = useMemo(() => {
     return {
@@ -100,8 +52,11 @@ export default function Image({ config }) {
 
   // api start
   const fetchData = async () => {
-    const res = await axios.get(finalSettings.api);
+    const res = await request.get(finalSettings.api);
     console.log('table res', res);
+    if (res?.data) {
+      setData(res.data);
+    }
   }
 
   useEffect(fetchData, [finalSettings.api]);
