243bc4f7d457435a96d94a7e7bc34a5b89117177
bruce_zhang <bruce_zhang@trendmicro.com>
2021-02-09 14:04:40 +0800

    update code editor

--- src/components/CodeEditor/index.js
+++ src/components/CodeEditor/index.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import CodeMirror from 'codemirror';
+import React from 'react';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
 
-export default function CodeEditor() {
+import styles from './styles.module.scss'; 
 
-  CodeEditor()
+export default function CodeEditor({ value, onChange }) {
 
   return (
     <div>
-      
+      <CodeMirror
+        value={value}
+        className={styles.editor}
+        options={{
+          mode: {
+            name: "javascript"
+          },
+          theme: 'material',
+          lineNumbers: true
+        }}
+        onChange={(editor, data, value) => {
+        }}
+      />
     </div>
   )
 }
