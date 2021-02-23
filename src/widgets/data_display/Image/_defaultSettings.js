b2cfd4255c6e321b08439ffdcb006cf9a585f568
bruce_zhang <bruce_zhang@trendmicro.com>
2021-02-23 17:24:12 +0800

    update grid container

--- src/widgets/data_display/Image/_defaultSettings.js
+++ src/widgets/data_display/Image/_defaultSettings.js
@@ -1,7 +1,7 @@
 export default {
-  src: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  src: 'https://images.unsplash.com/photo-1613982913259-f4625288f105?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2124&q=80',
   fallback: '',
-  width: '200px',
+  width: '100%',
   height: 'auto',
   alt: 'image',
 }
